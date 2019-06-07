470b38a66b11e652dddb329113930cdaf628172c
Felipe Valencia <fvalencia@vc-soft.com>
2019-06-06 19:36:36 -0500

    Ready to start Update Route

--- Flask/FlaskTutorial/flaskr/auth.py
+++ Flask/FlaskTutorial/flaskr/auth.py
@@ -28,7 +28,7 @@ def register():
         elif not password:
             error = 'Password is required'
         elif db.execute(
-                'SELECT id FROM user WHERE username = ?', (username,)
+                "SELECT id FROM user WHERE username = ?", (username,)
         ).fetchone() is not None:
             error = 'User {} is already registered'.format(username)
         if error is None:
@@ -78,5 +78,5 @@ def load_logged_in_user():
         g.user = None
     else:
         g.user = get_db().execute(
-            'SELECT * FROM user WHERE id = ?', (user_id)
+            'SELECT * FROM user WHERE id = ?', user_id,
         ).fetchone()
--- Flask/FlaskTutorial/flaskr/blog.py
+++ Flask/FlaskTutorial/flaskr/blog.py
@@ -1,21 +1,37 @@
 from werkzeug.exceptions import abort
-from flask import Blueprint, render_template, flash, url_for, redirect
+from flask import Blueprint, render_template, flash, url_for, redirect, request, g
 from flaskr.auth import login_required
 from flaskr.db import get_db
+
+
 def get_post(id, check_author=True):
-    post = get_db.execute(
-        'SELECT p.id, title, body, created, author_id, username FROM post p JOIN user ON p.author_id = u.id WHERE p.id = ?', (id,)
+    post = get_db().execute(
+        'SELECT p.id, title, body, created, author_id, username'
+        ' FROM post p JOIN user ON p.author_id = user.id'
+        ' WHERE p.id = ?',
+        (id,)
     ).fetchone()
+    if post is None:
+        abort(404, 'Post id {} does not exist'.format(id))
+    if check_author and post['author_id'] != g.user['id']:
+        abort(403)
+    return post
+
+
 app = Blueprint('blog', __name__)
+
+
 @app.route('/')
 def index():
     db = get_db()
     posts = db.execute(
-        'SELECT p.id, title, body, created, author_id, username FROM post p JOIN user u ON p.author_id = u.id ORDER by created DESC'
+        'SELECT p.id, title, body, created, author_id, username'
+        ' FROM post p JOIN user u ON p.author_id = u.id ORDER by created DESC'
     ).fetchall()
-    return render_template('blog/index.html', posts= posts)
+    return render_template('blog/index.html', posts=posts)
 
-@app.route('/create', methods=('GET','POST'))
+
+@app.route('/create', methods=('GET', 'POST'))
 @login_required
 def create():
     if request.method == 'POST':
@@ -29,8 +45,9 @@ def create():
         else:
             db = get_db()
             db.execute(
-                'INSERT INTO post (title, body, author_id) VALUES (?,?,?)', (title,body,g.user['id'])
+                'INSERT INTO post (title, body, author_id)'
+                ' VALUES (?,?,?)', (title, body, g.user['id'])
             )
             db.commit()
             return redirect(url_for('index'))
-    return render_template('blog/create.html)
+    return render_template('blog/create.html')
