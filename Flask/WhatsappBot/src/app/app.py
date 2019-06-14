be5fef2a556812731cc2a11541aea59dd6007a28
Felipe Valencia <fvalencia@vc-soft.com>
2019-06-13 20:57:14 -0500

    Started WhatsappBot

---
+++ Flask/WhatsappBot/src/app/app.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, request
+from twilio.rest import Client
+
+SID = "AC652c51e939f97d17a00c3105769649d7"
+KEY = "df7e3c41ef9a295051542cc50666c3ee"
+
+app = Flask(__name__)
+client = Client(SID, KEY)
+
+
+@app.route('/incoming', methods=['POST'])
+def incoming():
+    text = request.form['Body']
+    to = request.form['To']
+    origin = request.form['Form']
+    message = client.messages.create(
+        from_=origin,
+        body=text,
+        to=to
+    )
+    return str(message.error_code or 200)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
